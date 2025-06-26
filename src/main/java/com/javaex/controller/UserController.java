646c3210decd9c86eb0f34bd83084a811b8168fc
kangsb-010313 <kangsb668@naver.com>
2025-06-26 22:09:32 +0900

    ms 250626 22:09

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.javaex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.javaex.service.UserService;
+import com.javaex.vo.UserVO;
+
 @Controller
 public class UserController {
+	
+	//필드
+	@Autowired
+	private UserService userService;
 
 	//메소드일반
+	//회원가입폼
 	@RequestMapping(value="/user/joinform", method= {RequestMethod.GET, RequestMethod.POST})
 	public String joinForm() {
 		System.out.println("UserController.joinForm()");
@@ -16,6 +26,15 @@ public class UserController {
 		
 	}
 	
+	//회원가입
+	@RequestMapping(value="/user/joinok", method= {RequestMethod.GET, RequestMethod.POST})
+	public String joinOk(@ModelAttribute UserVO userVO) {
+		System.out.println("UserController.joinOk()");
+		
+		userService.exeJoin(userVO);
+		
+		return "user/joinok";
+	}
 	
 	
 }
--- src/main/java/com/javaex/repository/UserRepository.java
+++ src/main/java/com/javaex/repository/UserRepository.java
@@ -1,5 +1,25 @@
 package com.javaex.repository;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.UserVO;
+
+@Repository
 public class UserRepository {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int userInsert(UserVO userVO) {
+		
+		System.out.println("UserRepository.userInsert()");
+		
+		int count = sqlSession.insert("mysite.insert", userVO);
+		
+		return count;
+		
+	}
 
 }
