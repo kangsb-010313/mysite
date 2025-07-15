8d39893da4bd507e060fb5ff688f4876337ab966
kangsb-010313 <kangsb668@naver.com>
2025-07-15 17:06:43 +0900

    ms 250715 17:06 gallery

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -10,6 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
-        .addResourceLocations("file:///C:/javaStudy/upload/");
+        .addResourceLocations("file:/Users/kangsubin/Desktop/javaStudy/upload/");
+        // 윈도우 경로	-> file:///C:/javaStudy/upload/
+        // 맥 경로	-> /Users/kangsubin/Desktop/javaStudy/upload/
     }
 }
